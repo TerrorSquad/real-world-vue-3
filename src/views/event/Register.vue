fcc45090a083f02b35c0bccd46e7c7f7efa383eb
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-01-02 23:15:06 +0100

    Refactor import paths and add registration functionality

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,12 +1,20 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me!</button>
 </template>
 
 <script setup>
+import router from '@/router'
+
 const props = defineProps({
   event: {
     type: Object,
     required: true,
   },
 })
+
+const register = () => {
+  // if registration API call is successful
+  router.push({ name: 'EventDetails' })
+}
 </script>
