8caa2a6e056e815cfffdcb872812781b7979cf2b
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-01-05 16:17:19 +0100

    Update dependencies and add TypeScript support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp, reactive } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from '@/App.vue'
-import router from '@/router'
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
